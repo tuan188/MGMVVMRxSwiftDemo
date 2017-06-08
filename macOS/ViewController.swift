b18b45ee316da6127d35117842c8b2b6ea8d39b5
Tuan Truong <tuan188@gmail.com>
2017-06-08 15:31:58 +0700

    Add macOS target

---
+++ macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOS
+//
+//  Created by Tuan Truong on 6/8/17.
+//  Copyright © 2017 Tuan Truong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
